0b606a2a880edda1675e3ea49df9925cc0b37242
Ekta <ektapanjwani20@gmail.com>
2020-07-03 18:46:35 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,3 +1,3 @@
-truncate table public.circlecitest_lambda3
+truncate table public.circlecitest_lambda3;
 copy public.circlecitest_lambda3 from 's3://redshifttestbucket/category_object_paths.json' credentials 'aws_iam_role=arn:aws:iam::181935492667:role/MyTestRedshiftRole' json 's3://redshifttestbucket/category_jsonpath.json';
 select * from public.circlecitest_lambda3;
